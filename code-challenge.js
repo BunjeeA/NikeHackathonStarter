8ea61eb77c1ae28db4882725041bdd587b8dce39
BunjeeA <awoyomi.ab@gmail.com>
2022-09-24 16:15:44 -0400

    new group

--- code-challenge.js
+++ code-challenge.js
@@ -2,42 +2,63 @@
 const { SPANISH_DICTIONARY, SHOE_LIST } = require('./constants.js');
 
 const newSneakers = (sneakers) => {
-  return "New sneakers " + sneakers + " dropping soon!";
+  // return "New sneakers " + sneakers + " dropping soon!";
+  return `New sneakers ${sneakers} dropping soon!`
 };
 
 const isThatMichaelJordan = (name) => {
-  return "MICHAEL JORDAN" == name.toUpperCase()
+  // return "MICHAEL JORDAN" == name.toUpperCase()
+  return name.toLowerCase() == 'michael jordan' ? true : false
 
 };
 
 const areYouM = (name) => {
-  if (name.charAt(0) == "M") {
-    return true;
-  }
-  return false;
+  // if (name.charAt(0) == "M") {
+  //   return true;
+  // }
+  // return false;
+  return name.indexOf('M') == 0 ? true : false
 };
 
 const mostViewsThreeSneakers = (sneakerOneViews, sneakerTwoViews, sneakerThreeViews) => {
 
-  if (sneakerOneViews > sneakerTwoViews && sneakerOneViews > sneakerThreeViews) {
-    return sneakerOneViews;
-  }
-  else if (sneakerTwoViews > sneakerOneViews & sneakerTwoViews > sneakerThreeViews) {
-    return sneakerTwoViews;
-  }
-  else {
-    return sneakerThreeViews;
-  }
+  // if (sneakerOneViews > sneakerTwoViews && sneakerOneViews > sneakerThreeViews) {
+  //   return sneakerOneViews;
+  // }
+  // else if (sneakerTwoViews > sneakerOneViews & sneakerTwoViews > sneakerThreeViews) {
+  //   return sneakerTwoViews;
+  // }
+  // else {
+  //   return sneakerThreeViews;
+  // }
+    const arr = [sneakerOneViews, sneakerTwoViews, sneakerThreeViews]
+    let biggestNum = 0
+  
+    for(let i = 0; i < arr.length;i++){
+      if(arr[i] > biggestNum){
+        biggestNum = arr[i]
+      }
+    }
+    return biggestNum
 };
 
 const mostViews = (sneakerViews) => {
-  let max = 0
-  for (let i = 0; i < sneakerViews.length; i++) {
-    if (sneakerViews[i] > max) {
-      max = sneakerViews[i]
+  // let max = 0
+  // for (let i = 0; i < sneakerViews.length; i++) {
+  //   if (sneakerViews[i] > max) {
+  //     max = sneakerViews[i]
+  //   }
+  // }
+  // return max
+  const mostViews = (sneakerViews) => {
+    for(let i = 0; i < arr.length;i++){
+      if(arr[i] > biggestNum){
+        biggestNum = arr[i]
+      }
     }
-  }
-  return max
+    return biggestNum
+  };
+
 };
 
 const validateEmail = (email) => {
