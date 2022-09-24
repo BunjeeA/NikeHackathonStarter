3d9f31f4e9a38cef6c198b749ac92d9a22f0f393
BunjeeA <awoyomi.ab@gmail.com>
2022-09-24 16:20:27 -0400

    OGgroup

--- code-challenge.js
+++ code-challenge.js
@@ -2,60 +2,42 @@
 const { SPANISH_DICTIONARY, SHOE_LIST } = require('./constants.js');
 
 const newSneakers = (sneakers) => {
-  // return "New sneakers " + sneakers + " dropping soon!";
-  return `New sneakers ${sneakers} dropping soon!`
+  return "New sneakers " + sneakers + " dropping soon!";
 };
 
 const isThatMichaelJordan = (name) => {
-  // return "MICHAEL JORDAN" == name.toUpperCase()
-  return name.toLowerCase() == 'michael jordan' ? true : false
+  return "MICHAEL JORDAN" == name.toUpperCase()
 
 };
 
 const areYouM = (name) => {
-  // if (name.charAt(0) == "M") {
-  //   return true;
-  // }
-  // return false;
-  return name.indexOf('M') == 0 ? true : false
+  if (name.charAt(0) == "M") {
+    return true;
+  }
+  return false;
 };
 
 const mostViewsThreeSneakers = (sneakerOneViews, sneakerTwoViews, sneakerThreeViews) => {
 
-  // if (sneakerOneViews > sneakerTwoViews && sneakerOneViews > sneakerThreeViews) {
-  //   return sneakerOneViews;
-  // }
-  // else if (sneakerTwoViews > sneakerOneViews & sneakerTwoViews > sneakerThreeViews) {
-  //   return sneakerTwoViews;
-  // }
-  // else {
-  //   return sneakerThreeViews;
-  // }
-    const arr = [sneakerOneViews, sneakerTwoViews, sneakerThreeViews]
-    let biggestNum = 0
-  
-    for(let i = 0; i < arr.length;i++){
-      if(arr[i] > biggestNum){
-        biggestNum = arr[i]
-      }
-    }
-    return biggestNum
+  if (sneakerOneViews > sneakerTwoViews && sneakerOneViews > sneakerThreeViews) {
+    return sneakerOneViews;
+  }
+  else if (sneakerTwoViews > sneakerOneViews & sneakerTwoViews > sneakerThreeViews) {
+    return sneakerTwoViews;
+  }
+  else {
+    return sneakerThreeViews;
+  }
 };
 
 const mostViews = (sneakerViews) => {
-  // let max = 0
-  // for (let i = 0; i < sneakerViews.length; i++) {
-  //   if (sneakerViews[i] > max) {
-  //     max = sneakerViews[i]
-  //   }
-  // }
-  // return max
-    for(let i = 0; i < arr.length;i++){
-      if(arr[i] > biggestNum){
-        biggestNum = arr[i]
-      }
+  let max = 0
+  for (let i = 0; i < sneakerViews.length; i++) {
+    if (sneakerViews[i] > max) {
+      max = sneakerViews[i]
     }
-    return biggestNum
+  }
+  return max
 };
 
 const validateEmail = (email) => {
